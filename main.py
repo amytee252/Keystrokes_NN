0f84bbf61c242eb2abcbf92ff49da804bd360ee3
amytee252 <amytee252@hotmail.com>
2022-04-28 11:52:49 +0100

    updates to README

---
+++ main.py
@@ -0,0 +1,14 @@
+# Keystroke Dynamics Analysis and Prediction
+
+import numpy as np
+import pandas as pd
+
+import matplotlib as plt
+
+import seaborn as sns
+
+
+df = pd.read_csv("./dataset/DSL-StrongPasswordData.csv") #open csv file for reading in pandas dataframe
+
+print(df.info()) #Get info about dataframe
+print(df.head()) #look at first handful of entries to know what we are dealing with
